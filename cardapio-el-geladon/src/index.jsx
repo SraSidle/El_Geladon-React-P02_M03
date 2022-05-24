c5768f4aa23857620cfa6ac194ca29b70d5d1ada
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-24 20:23:58 -0300

    criacao, estilizacao e conexao com outros arquivos(geral)

--- cardapio-el-geladon/src/index.jsx
+++ cardapio-el-geladon/src/index.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import reactDOM from 'react-dom';
+import React from "react";
+import reactDOM from "react-dom";
+import Home from "./components/Home";
+import "./components/Index.css"; //Tem que ser com a primeira letra maiúscula, sempre, SEMPRE
 
 reactDOM.render(
-    <React.StrictMode>Hello World!</React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
