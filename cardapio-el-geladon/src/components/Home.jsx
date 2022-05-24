a5946d9869e0f83f8b6abb5ca9df16534b6d5bc4
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-24 20:24:32 -0300

    criacao da pasta inicial e estilizacao primaria

---
+++ cardapio-el-geladon/src/components/Home.jsx
@@ -0,0 +1,7 @@
+import '../components/Home.css'
+
+function Home() {
+  return <div className="Home">Este é o componente Home</div>;
+} // Todo componente precisa ser exportado e aqui é "className"
+
+export default Home;
