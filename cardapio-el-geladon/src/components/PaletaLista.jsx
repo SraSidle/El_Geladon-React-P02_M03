c02e432ec8f0dccabd7cefa475679c3d1a58bbdf
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-24 23:19:55 -0300

    listagem das paletas em memória e estilizacao

---
+++ cardapio-el-geladon/src/components/PaletaLista.jsx
@@ -0,0 +1,27 @@
+import { paletas } from "./mocks/Paletas";
+import "./PaletaLista.css";
+
+function PaletaLista() {
+  return <div className="PaletaLista">
+      {paletas.map((paleta, index) => (
+             <div className="PaletaListaItem">
+                 <div>
+                     <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
+                     <div className="PaletaListaItem__preco">R$ {paleta.preco.toFixed(2)}</div>
+                     <div className="PaletaListaItem__descricao">{paleta.descricao}</div>
+                     <div className="PaletaListaItem__acoes Acoes">
+                          <button className="Acoes__adicionar Acoes__adicionar-_preencher">Adicionar</button>                 
+                     </div>
+                 </div>
+                 <img
+                   className="PaletaListaItem__foto"
+                   src={paleta.foto}
+                   alt={`Paleta de ${paleta.sabor}`}
+                 />
+             </div>
+))};
+
+         </div>;
+}
+
+export default PaletaLista;
