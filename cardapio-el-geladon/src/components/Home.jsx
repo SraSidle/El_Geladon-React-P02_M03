dd40e27b688cbf8b86a1bfffeeb70b935708ebaf
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-24 23:20:27 -0300

    adiciona imports e estilizacao

--- cardapio-el-geladon/src/components/Home.jsx
+++ cardapio-el-geladon/src/components/Home.jsx
@@ -1,7 +1,14 @@
-import '../components/Home.css'
+import PaletaLista from "./PaletaLista"; //Parece que se vc for citar o elemnto exportado, tem que declará-lo, junto com a exportação
+import "../components/Home.css";
 
 function Home() {
-  return <div className="Home">Este é o componente Home</div>;
+  return (
+    <div className="Home">
+      <div className="Home__container">
+        <PaletaLista />
+      </div>
+    </div>
+  );
 } // Todo componente precisa ser exportado e aqui é "className"
 
 export default Home;
