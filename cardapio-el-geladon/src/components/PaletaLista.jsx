d7361ce9d363da115e1bd5012d532482591346e5
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-25 21:33:12 -0300

    configuracao, porem nao esta renderizando

--- cardapio-el-geladon/src/components/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista.jsx
@@ -1,16 +1,31 @@
+import React, {useState} from 'react';
 import { paletas } from "./mocks/Paletas";
 import "./PaletaLista.css";
 
 function PaletaLista() {
+
+  const {paletaSelecionada , setPaletaSelecionada} = useState({});
+
+  const adicionarItem = (paletaIndex) => {
+    const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) +1}
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta});
+
+    return
+  }
   return <div className="PaletaLista">
       {paletas.map((paleta, index) => (
              <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
+               <span className="PaletaListaIitem__badge">{paletaSelecionada[index] || 0}</span>
                  <div>
                      <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
                      <div className="PaletaListaItem__preco">R$ {paleta.preco.toFixed(2)}</div>
                      <div className="PaletaListaItem__descricao">{paleta.descricao}</div>
                      <div className="PaletaListaItem__acoes Acoes">
-                          <button className="Acoes__adicionar Acoes__adicionar-_preencher">Adicionar</button>                 
+                          <button 
+                            className="Acoes__adicionar Acoes__adicionar-_preencher"
+                            onClick={() => adicionarItem(index)}>
+                              Adicionar
+                          </button>                 
                      </div>
                  </div>
                  <img
