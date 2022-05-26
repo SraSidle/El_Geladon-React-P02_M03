51f1d870b8c793b456de9311f6566d1910e7c6fd
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-26 07:44:42 -0300

    correcaao do bug do reacDOM adicionando root

--- cardapio-el-geladon/src/index.jsx
+++ cardapio-el-geladon/src/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import reactDOM from "react-dom";
+import reactDOM from "react-dom/client"; // react-dom não suportado na versão 18, utilizar "createRoot" e colocar o /client nessa linha
 import Home from "./components/Home";
 import "./components/Index.css"; //Tem que ser com a primeira letra maiúscula, sempre, SEMPRE
 
-reactDOM.render(
+const root = reactDOM.createRoot(document.getElementById('root'));
+// adciona essa linha de código, para criar o root e utilizar para redenrização, e não o "reactDOM"
+
+root.render( //troca o reactDOM presente aqui, por "root"
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
-  document.getElementById("root")
+  // retira a linha que estava aqui, pois ela é citada na linha 7
 );
 
 //manifest.json é utilizado quando se trabalha com TWA(geração de ícones para a aplicação)/ funciona com app nativo
