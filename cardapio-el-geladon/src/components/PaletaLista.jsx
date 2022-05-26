0b508b1c5a2d23e149d3b4b0e484114b9b82aa7e
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-26 07:45:09 -0300

    retirada da linha que dava erro para posterior correcao

--- cardapio-el-geladon/src/components/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista.jsx
@@ -7,15 +7,18 @@ function PaletaLista() {
   const {paletaSelecionada , setPaletaSelecionada} = useState({});
 
   const adicionarItem = (paletaIndex) => {
-    const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) +1}
+    const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex]) +1}
+    console.log("paletaIndex" ,paletaIndex)
     setPaletaSelecionada({ ...paletaSelecionada, ...paleta});
+    console.log("paletaSelecionada:" , paletaSelecionada) 
 
-    return
   }
-  return <div className="PaletaLista">
+return <div className="PaletaLista">
       {paletas.map((paleta, index) => (
-             <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
-               <span className="PaletaListaIitem__badge">{paletaSelecionada[index] || 0}</span>
+             <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>   
+             {/*  <span className="PaletaListaIitem__badge">{paletaSelecionada[index]}</span>
+              Essa linha dá erro, toda vez, não redenriza as paletas na tela, afirma sobre propriedades "undefined" e nulas
+             */}
                  <div>
                      <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
                      <div className="PaletaListaItem__preco">R$ {paleta.preco.toFixed(2)}</div>
