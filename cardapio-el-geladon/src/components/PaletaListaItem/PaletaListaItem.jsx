a621c2061d346796ccef3f2378445d40e983e5a2
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-29 19:55:32 -0300

    linha18, adicionando um valor padrao como espaco

--- cardapio-el-geladon/src/components/PaletaListaItem/PaletaListaItem.jsx
+++ cardapio-el-geladon/src/components/PaletaListaItem/PaletaListaItem.jsx
@@ -1,34 +1,41 @@
 import "../PaletaListaItem/PaletaListaItem";
+import React from "react";
+import "../PaletaListaItem/PaletaListaItem.css";
 
 function PaletaListaItem({
   paleta,
   quantidadeSelecionada,
   index,
-  onRemove,
-  onAdd,
+  adicionarItem,
+  removerItem,
 }) {
+
   const badgeCounter = (canRender, index) => {
-    Boolean(canRender) && (
-      <span className="PaletaListaItem__badge"> {quantidadeSelecionada}</span>
+    return (
+      <>
+        {Boolean(canRender) && (
+          <span className="PaletaListaItem__badge">
+            {" "}
+            {quantidadeSelecionada}
+          </span>
+        )}
+      </>
     );
   };
 
-  const removeButton = (canRender, index) => {
-    Boolean(canRender) && (
-      <button className="Acoes__remover" onClick={() => onRemove[index]}>
-        Remover
-      </button>
-    );
-  };
-
+  const removeButton = (canRender, index) => (
+    <>
+      {Boolean(canRender) && (
+        <button className="Acoes__remover" onClick={() => removerItem(index)}>
+          Remover
+        </button>
+      )}
+    </>
+  );
+  
   return (
     <div className="PaletaListaItem">
-      {/* {badgeCounter(paletaSelecionada[index], index)} 
-          Essa linha se comporta de maneira semelhante ao abaixo, produzindo erro
-        */}
-      {/*  <span className="PaletaListaIitem__badge">{paletaSelecionada[index]}</span>
-            Essa linha dá erro, toda vez, não redenriza as paletas na tela, afirma sobre propriedades "undefined" e nulas
-           */}
+      {badgeCounter(quantidadeSelecionada, index)}
       <div>
         <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
         <div className="PaletaListaItem__preco">
@@ -40,13 +47,11 @@ function PaletaListaItem({
             className={`Acoes__adicionar ${
               !quantidadeSelecionada && "Acoes__adicionar-_preencher"
             }`}
-            onClick={() => onAdd[index]}
+            onClick={() => adicionarItem(index)}
           >
-            {removeButton(quantidadeSelecionada)}
-            {/*{removeButton(paletaSelecionada[index], index)} // erro dnv*/}
-            {/* O problema está no index, toda vez que eu retiro ele, redenriza novamente "paletaSelecionada[index]" linha 44*/}
             Adicionar
           </button>
+          {removeButton(quantidadeSelecionada, index)}
         </div>
       </div>
       <img
